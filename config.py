9a316f4d6706a0f49d98a2cb1cb018137013a285
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-06-15 20:55:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,16 +27,23 @@ for x in Bctchinna:
 OWNER_ID = int(getenv("OWNER_ID", default="5518687442"))
 SUDO_USERS.append(OWNER_ID)
 
+START_MESSAGE = getenv("START_MESSAGE", None)
 
-# ------------- CLIENTS -------------
+PING_PIC = getenv("PING_PIC", None)
 
-X1 = TelegramClient('X1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-X2 = TelegramClient('X2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-X3 = TelegramClient('X3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-X4 = TelegramClient('X4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-X5 = TelegramClient('X5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-X6 = TelegramClient('X6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-X7 = TelegramClient('X7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-X8 = TelegramClient('X8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-X9 = TelegramClient('X9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-X10 = TelegramClient('X10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+START_PIC = getenv("START_PIC", None)
+
+OWNER_ID = int(getenv("OWNER_ID", default="7009601543"))
+HELP_MSG = getenv("HELP_MSG", None)
+HELP_PIC = getenv("HELP_PIC", "https://telegra.ph/file/c26f985c3f59004bc9927.jpg")
+LOG_CHANNEL = getenv("LOG_CHANNEL", None)
+
+HANDLER = getenv("HANDLER", "/")
+
+# SUDO and OWNER logic
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "7762101994").split()))
+SUDO_USERS.extend(BAD)  # Add users from your custom list
+OWNER_ID = int(os.getenv("OWNER_ID", "7762101994"))
+if OWNER_ID not in SUDO_USERS:
+    SUDO_USERS.append(OWNER_ID)
+SUDO_USERS = list(set(SUDO_USERS))  # remove duplicates
